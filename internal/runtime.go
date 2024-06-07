29ea72176bce750cde6a5db0056440fa1536493d
ExoMonk <0xexomonk@gmail.com>
2024-06-07 17:48:00 +0100

    refactor: prediction pipeline

--- internal/runtime.go
+++ internal/runtime.go
@@ -11,3 +11,4 @@
 // 2024-06-05T17:02:00 fix: settlement race condition
 // 2024-06-07T15:58:00 fix: oracle feed handler
 // 2024-06-07T16:29:00 feat: position sizing
+// 2024-06-07T17:48:00 refactor: prediction pipeline
