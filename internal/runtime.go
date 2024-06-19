1cf17ac6d94a6fbec332a174af2f9700e43a5ee9
ExoMonk <0xexomonk@gmail.com>
2024-06-19 21:46:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -36,3 +36,4 @@
 // 2024-06-17T12:21:00 refactor: order matching
 // 2024-06-17T09:03:00 fix: settlement race condition
 // 2024-06-17T17:06:00 fix: oracle feed handler
+// 2024-06-19T21:46:00 feat: market resolver
