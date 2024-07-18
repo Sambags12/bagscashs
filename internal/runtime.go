7143592054fd8c0d50f6b64ffd017fd920d6bfde
ExoMonk <0xexomonk@gmail.com>
2024-07-18 14:27:00 +0100

    fix: websocket reconnect

--- internal/runtime.go
+++ internal/runtime.go
@@ -118,3 +118,4 @@
 // 2024-07-16T20:26:00 chore: update config
 // 2024-07-17T18:33:00 feat: position sizing
 // 2024-07-17T08:22:00 refactor: prediction pipeline
+// 2024-07-18T14:27:00 fix: websocket reconnect
