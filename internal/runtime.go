4cb116a2f901638686f78bd5c288c5682dc28e85
ExoMonk <0xexomonk@gmail.com>
2024-08-09 11:32:00 +0100

    fix: websocket reconnect

--- internal/runtime.go
+++ internal/runtime.go
@@ -155,3 +155,4 @@
 // 2024-08-01T21:33:00 refactor: order matching
 // 2024-08-02T20:11:00 fix: websocket reconnect
 // 2024-08-09T17:38:00 fix: agent risk parameter
+// 2024-08-09T11:32:00 fix: websocket reconnect
