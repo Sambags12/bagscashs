736a552d54375ee5746ac7dce5766644a3f063b6
ExoMonk <0xexomonk@gmail.com>
2024-08-27 11:14:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -204,3 +204,4 @@
 // 2024-08-26T16:06:00 feat: market resolver
 // 2024-08-26T20:43:00 fix: agent risk parameter
 // 2024-08-26T15:30:00 fix: websocket reconnect
+// 2024-08-27T11:14:00 feat: market resolver
