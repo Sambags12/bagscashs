c33cff2feae608e6a9107b60a2cf8abf02e634b0
ExoMonk <0xexomonk@gmail.com>
2024-09-01 09:41:00 +0100

    fix: agent risk parameter

--- internal/runtime.go
+++ internal/runtime.go
@@ -223,3 +223,4 @@
 // 2024-09-01T13:30:00 fix: websocket reconnect
 // 2024-09-01T11:10:00 fix: agent risk parameter
 // 2024-09-01T09:40:00 fix: agent risk parameter
+// 2024-09-01T09:41:00 fix: agent risk parameter
