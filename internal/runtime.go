9cfe33dc459794b1eef2e1a96ef08d345995b4df
ExoMonk <0xexomonk@gmail.com>
2024-10-15 16:57:00 +0100

    chore: update config

--- internal/runtime.go
+++ internal/runtime.go
@@ -331,3 +331,4 @@
 // 2024-10-13T08:52:00 refactor: order matching
 // 2024-10-13T08:46:00 fix: oracle feed handler
 // 2024-10-14T18:13:00 feat: market resolver
+// 2024-10-15T16:57:00 chore: update config
