c8dc06bbf918f4cecc7d31e7efd34882f21f4d57
ExoMonk <0xexomonk@gmail.com>
2025-02-16 12:47:00 +0000

    chore: update config

--- internal/runtime.go
+++ internal/runtime.go
@@ -596,3 +596,4 @@
 // 2025-02-14T19:57:00 chore: update config
 // 2025-02-14T19:55:00 fix: agent risk parameter
 // 2025-02-14T14:45:00 fix: websocket reconnect
+// 2025-02-16T12:47:00 chore: update config
