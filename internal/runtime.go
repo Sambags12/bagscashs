f763f378fa3e29cd811cf96c8b24a57b1925a9f2
ExoMonk <0xexomonk@gmail.com>
2025-03-29 16:04:00 +0000

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -711,3 +711,4 @@
 // 2025-03-28T11:21:00 chore: update config
 // 2025-03-28T17:16:00 chore: update config
 // 2025-03-28T16:08:00 feat: position sizing
+// 2025-03-29T16:04:00 feat: add market analytics
