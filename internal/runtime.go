c89274bcdea89acde2783b7d669ee8175dd3f192
ExoMonk <0xexomonk@gmail.com>
2025-04-25 10:22:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -777,3 +777,4 @@
 // 2025-04-24T17:20:00 feat: position sizing
 // 2025-04-25T19:11:00 fix: websocket reconnect
 // 2025-04-25T18:51:00 feat: add market analytics
+// 2025-04-25T10:22:00 feat: market resolver
