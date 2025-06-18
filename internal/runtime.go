c463af6a165f0f088a7b61ad9e23e67d110f40b8
ExoMonk <0xexomonk@gmail.com>
2025-06-18 17:28:00 +0100

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -888,3 +888,4 @@
 // 2025-06-17T16:52:00 chore: update config
 // 2025-06-18T14:00:00 fix: settlement race condition
 // 2025-06-18T11:22:00 fix: oracle feed handler
+// 2025-06-18T17:28:00 feat: add market analytics
