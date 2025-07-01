cd4b66d863ab9d16a5cfe7abffa18a7e400e4d28
ExoMonk <0xexomonk@gmail.com>
2025-07-01 10:07:00 +0100

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -922,3 +922,4 @@
 // 2025-07-01T13:47:00 fix: websocket reconnect
 // 2025-07-01T21:47:00 fix: settlement race condition
 // 2025-07-01T17:10:00 fix: settlement race condition
+// 2025-07-01T10:07:00 feat: add market analytics
