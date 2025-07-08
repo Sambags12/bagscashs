83d73d903bb68e4aa2571c3500be5eec0bf18005
ExoMonk <0xexomonk@gmail.com>
2025-07-08 20:51:00 +0100

    fix: settlement race condition

--- internal/runtime.go
+++ internal/runtime.go
@@ -938,3 +938,4 @@
 // 2025-07-05T18:00:00 chore: update config
 // 2025-07-05T18:20:00 refactor: prediction pipeline
 // 2025-07-08T09:20:00 fix: settlement race condition
+// 2025-07-08T20:51:00 fix: settlement race condition
