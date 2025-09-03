458a28e57ca09ea77b2d0601b97c3844ed3a57bb
ExoMonk <0xexomonk@gmail.com>
2025-09-03 21:28:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -1062,3 +1062,4 @@
 // 2025-09-01T12:39:00 feat: add market analytics
 // 2025-09-02T13:21:00 refactor: prediction pipeline
 // 2025-09-02T21:58:00 feat: position sizing
+// 2025-09-03T21:28:00 feat: market resolver
