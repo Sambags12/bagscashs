07343bb78b4ee87a6171b35d487983a876a35e0c
ExoMonk <0xexomonk@gmail.com>
2025-09-18 21:57:00 +0100

    fix: websocket reconnect

--- internal/runtime.go
+++ internal/runtime.go
@@ -1104,3 +1104,4 @@
 // 2025-09-14T17:22:00 feat: add market analytics
 // 2025-09-14T11:53:00 fix: websocket reconnect
 // 2025-09-18T17:46:00 fix: websocket reconnect
+// 2025-09-18T21:57:00 fix: websocket reconnect
