bb7d89691bc62ee14a57f5844e71781d07138654
ExoMonk <0xexomonk@gmail.com>
2025-10-24 10:12:00 +0100

    chore: update config

--- internal/runtime.go
+++ internal/runtime.go
@@ -1178,3 +1178,4 @@
 // 2025-10-22T19:54:00 chore: update config
 // 2025-10-23T10:23:00 feat: add market analytics
 // 2025-10-23T17:43:00 refactor: order matching
+// 2025-10-24T10:12:00 chore: update config
